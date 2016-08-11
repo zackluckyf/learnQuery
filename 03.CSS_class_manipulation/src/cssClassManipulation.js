9473d5b06fd4424b36f029d4bd43615a8e78363f
zackluckyf <fanning.34@gmail.com>
2016-08-11 11:29:00 -0400

    03 CSS finished and 04 DOM finished

--- 03.CSS_class_manipulation/src/cssClassManipulation.js
+++ 03.CSS_class_manipulation/src/cssClassManipulation.js
@@ -1,5 +1,14 @@
-var cssClass = (function() {
-  'use strict';
-
-  // code goes here
-})();
+var cssClass = {
+  add: function (element, className) {
+    element.classList.add(className);
+  },
+  remove: function (element, className) {
+    element.classList.remove(className);
+  },
+  toggle: function (element, className) {
+    element.classList.toggle(className);
+  },
+  has: function (element, className) {
+    return element.classList.contains(className) ? true : false;
+  }
+};
--- 04.DOM_manipulation/src/domManipulation.js
+++ 04.DOM_manipulation/src/domManipulation.js
@@ -1,3 +1,29 @@
-var dom = function(){
-  // code goes here
-};
+var dom = (function () {
+  var notFound = '404 not found';
+  return {  remove: function (element) {
+      if (!document.contains(element)) {
+        return notFound;
+      }
+      element.parentNode.removeChild(element);
+    },
+    append: function (element, newElement) {
+      element.appendChild(newElement);
+    },
+    prepend: function (element, newElement) {
+      element.insertBefore(newElement, element.firstChild);
+    },
+    after: function (element, newElement) {
+      if (!document.contains(element)) {
+        return notFound;
+      }
+      element.parentNode.insertBefore(newElement, element.nextSibling);
+    },
+
+    before: function (element, newElement) {
+      element.parentNode.insertBefore(newElement, element);
+    },
+    val: function (element) {
+      return element.value;
+    }
+  };
+})();
